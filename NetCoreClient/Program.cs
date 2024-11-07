ee468f66bfeffce62fb0684801dadab4cbfd8cc5
federicoburello <157379717+federicoburello@users.noreply.github.com>
2024-11-07 12:40:22 +0100

    commit client files

---
+++ NetCoreClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using NetCoreClient.Sensors;
+using NetCoreClient.Protocols;
+
+// define sensors
+List<ISensorInterface> sensors = new();
+sensors.Add(new VirtualWaterTempSensor());
+sensors.Add(new VirtualWaterStatusSensor());
+sensors.Add(new VirtualWaterUsageSensor());
+sensors.Add(new VirtualFilterWearSensor());
+
+// define protocol
+ProtocolInterface protocol = new Http("http://localhost:8011/water_coolers/123");
+
+// send data to server
+while (true)
+{
+    foreach (ISensorInterface sensor in sensors)
+    {
+        var sensorValue = sensor.ToJson();
+
+        protocol.Send(sensorValue);
+
+        Console.WriteLine("Data sent: " + sensorValue);
+
+        Thread.Sleep(1000);
+    }
+
+}
