6d96709458e37fb65f20716d3a5bdc23157120b6
its-rob <roberto.mio@stud.itsaltoadriatico.it>
2024-12-12 13:04:13 +0100

    Add AMQP server code

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+string CloudAMQPUrl = Environment.GetEnvironmentVariable("RABBITMQ_URI");
+
+try
+{
+    var factory = new ConnectionFactory
+    {
+        Uri = new Uri(CloudAMQPUrl)
+    };
+    using var connection = await factory.CreateConnectionAsync();
+    using var channel = await connection.CreateChannelAsync();
+
+    await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+        arguments: null);
+
+    Console.WriteLine(" [*] Waiting for messages.");
+
+    var consumer = new AsyncEventingBasicConsumer(channel);
+    consumer.ReceivedAsync += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($" [x] Received {message}");
+        return Task.CompletedTask;
+    };
+
+    await channel.BasicConsumeAsync("hello", autoAck: true, consumer: consumer);
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
+catch (Exception e)
+{
+    Console.WriteLine("ECCEZIONE!");
+    Console.WriteLine(e);
+}
